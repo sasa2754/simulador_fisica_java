8b689cee5f013c9aa879cccd90a0ad2c6db19f78
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-10-15 11:52:54 -0300

    mola

--- src/main/java/com/desktopapp/CanvaController.java
+++ src/main/java/com/desktopapp/CanvaController.java
@@ -15,22 +15,40 @@ import java.util.TimerTask;
 
 public class CanvaController implements Initializable {
 
-    
+
+    @FXML
+    public Canvas canva;
+
     public static Scene CreateScene() throws Exception {
         URL sceneUrl = CanvaController.class.getResource("CanvaScene.fxml");
         
         FXMLLoader loader = new FXMLLoader(sceneUrl);
         Parent root = loader.load();
+        Scene scene = new Scene(root);
 
-        return new Scene(root);
+        CanvaController controller = loader.getController();
+
+        Timer timerScene = new Timer();
+
+        timerScene.scheduleAtFixedRate(new TimerTask() {
+            
+            @Override
+            public void run(){
+                controller.canva.setWidth(scene.getWidth());
+                controller.canva.setHeight(scene.getHeight());
+            }
+        }, 50, 50);
+        
+        return scene;
     }
-
-    @FXML
-    public Canvas canva;
 
     protected Timer timer = new Timer();
 
     protected Integer numBolas = 100;
+
+    protected Integer numMolas = numBolas / 2;
+
+    public ArrayList<Mola> molas = new ArrayList<Mola>();
 
     public ArrayList<Mass> bolas = new ArrayList<Mass>();
     
@@ -44,7 +62,13 @@ public class CanvaController implements Initializable {
         tela = canva.getGraphicsContext2D();
 
         for (int i = 0; i < numBolas; i++) {
-            bolas.add(new Mass(5d, Math.random() * 1266 + 50, Math.random() * 668 - 50, Math.random() * 60 - 30, 0d, canva.getHeight(), canva.getWidth(), colors[(int)(Math.random() * (double)colors.length)]));
+            bolas.add(new Mass(Math.random() * 110 + 2, Math.random() * 1266 + 50, Math.random() * 668 - 50, Math.random() * 60 - 30, 0d, canva.getHeight(), canva.getWidth(), Math.random() * 75 + 25,colors[(int)(Math.random() * (double)colors.length)]));
+        }
+
+        bolas.get(0).color = Color.DARKORANGE;
+
+        for (int i = 0; i < numMolas; i++) {
+            molas.add(new Mola(bolas.get(i * 2), bolas.get(i * 2 + 1)));
         }
         
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -52,25 +76,28 @@ public class CanvaController implements Initializable {
             @Override
             public void run(){
                 tela.clearRect(0, 0, canva.getWidth(), canva.getHeight());
+                
+                for (int i = 0; i < numMolas; i++) {
+                    bolas.get(i * 2).ballPhysics(3d, 0.8, Math.random() * 20, canva.getHeight(), canva.getWidth());
+                    bolas.get(i * 2 + 1).ballPhysics(3d, 0.8, Math.random() * 20, canva.getHeight(), canva.getWidth());
+                    molas.get(i).springPhysics();
 
-                for (int i = 0; i < numBolas; i++) {
-                    drawBolinha(bolas.get(i).getX(), bolas.get(i).getY(), bolas.get(i).getColor());
-                    bolas.get(i).ballPhysics(3d, 0.9, Math.random() * 20);
+                    drawBolinha(bolas.get(i * 2).getX(), bolas.get(i * 2).getY(), bolas.get(i * 2).getSize(),bolas.get(i * 2).getColor());
+                    drawBolinha(bolas.get(i * 2 + 1).getX(), bolas.get(i * 2 + 1).getY(), bolas.get(i * 2 + 1).getSize(),bolas.get(i * 2 + 1).getColor());
+                    drawMolinha(molas.get(i).getX1(), molas.get(i).getY1(), molas.get(i).getX2(), molas.get(i).getY2(), Color.BLACK);
                 }
-
             }
         }, 60, 30);
     }
 
-    private void drawBolinha(Double x, Double y, Color cor){
+    private void drawBolinha(Double x, Double y, Double size, Color cor){
         tela.setFill(cor);
 
-        tela.fillRoundRect(x, y, 50, 50, 50, 50);
+        tela.fillRoundRect(x, y, size, size, size, size);
     }
 
-    private void drawMolinha(Double x, Double y, Color cor){
+    private void drawMolinha(Double x1, Double y1, Double x2, Double y2, Color cor){
         tela.setFill(cor);
-        tela.fillRect(x, y, );
-        
+        tela.strokeLine(x1, y1, x2, y2);
     }
 }
--- src/main/java/com/desktopapp/Mola.java
+++ src/main/java/com/desktopapp/Mola.java
@@ -2,19 +2,117 @@ package com.desktopapp;
 
 public class Mola {
     
-    protected Mass bola1;
-    protected Mass bola2;
+    protected Mass bola1, bola2;
+    protected Double x1, y1, x2, y2, midW, power;
+
+    public Double getPower() {
+        return power;
+    }
+
+    public void setPower(Double power) {
+        this.power = power;
+    }
+
+    public Double getMidW() {
+        return midW;
+    }
+
+    public void setMidW(Double midW) {
+        this.midW = midW;
+    }
+
+    public Double getX1() {
+        return x1;
+    }
+
+    public void setX1(Double x1) {
+        this.x1 = x1;
+    }
+
+    public Double getY1() {
+        return y1;
+    }
+
+    public void setY1(Double y1) {
+        this.y1 = y1;
+    }
+
+    public Double getX2() {
+        return x2;
+    }
+
+    public void setX2(Double x2) {
+        this.x2 = x2;
+    }
+
+    public Double getY2() {
+        return y2;
+    }
+
+    public void setY2(Double y2) {
+        this.y2 = y2;
+    }
+
+    public Mass getBola1() {
+        return bola1;
+    }
+
+    public void setBola1(Mass bola1) {
+        this.bola1 = bola1;
+    }
+
+    public Mass getBola2() {
+        return bola2;
+    }
+
+    public void setBola2(Mass bola2) {
+        this.bola2 = bola2;
+    }
 
     protected Mola(Mass bola1, Mass bola2){
         this.bola1 = bola1;
         this.bola2 = bola2;
+        this.midW = 400d;
     }
 
-    private void LinhaMola(Mass bola1, Mass bola2) {
-        var x1 = bola1.getX() + 25;
-        var y1 = bola1.getY() + 25;
+    public void springPhysics(){
+        setX1(getBola1().getX() + getBola1().getSize() / 2);
+        setY1(getBola1().getY() + getBola1().getSize() / 2);
 
-        var x2 = bola2.getX() + 25;
-        var y2 = bola2.getY() + 25;
+        setX2(getBola2().getX() + getBola2().getSize() / 2);
+        setY2(getBola2().getY() + getBola2().getSize() / 2);
+
+        Double Y = getBola1().getY() - getBola2().getY();
+        Double X = getBola1().getX() - getBola2().getX();
+
+        Double hyp = Math.hypot(getBola1().getY() - getBola2().getY(), getBola1().getX() - getBola2().getX());
+
+        
+        Double angle = Math.asin((getBola1().getY() - getBola2().getY()) / hyp) * 180 / Math.PI;
+        
+
+        setPower((hyp - midW) * 0.02);
+
+        Double weightSum = getBola1().getKg() + getBola2().getKg();
+
+        Double powerVelBall1 = getBola1().getKg() / weightSum * getPower();
+        Double powerVelBall2 = getBola2().getKg() / weightSum * getPower();
+        Double DisSum = Math.abs(Y) + Math.abs(X);
+        Double powerVelBall1x = powerVelBall1 * (Math.abs(X) / DisSum);
+        Double powerVelBall1y = powerVelBall1 * (Math.abs(Y) / DisSum);
+        Double powerVelBall2x = powerVelBall2 * (Math.abs(X) / DisSum);
+        Double powerVelBall2y = powerVelBall2 * (Math.abs(Y) / DisSum);
+        
+        if (getBola1().getX() < getBola2().getX()) {
+            getBola1().setVx(powerVelBall1x + getBola1().getVx());
+            getBola1().setVy(powerVelBall1y + getBola1().getVy());
+            getBola2().setVx(-(powerVelBall2x) + getBola2().getVx());
+            getBola2().setVy(-(powerVelBall2y) + getBola2().getVy());
+        } else {
+            getBola1().setVx(-(powerVelBall1x) + getBola1().getVx());
+            getBola1().setVy(-(powerVelBall1y) + getBola1().getVy());
+            getBola2().setVx(powerVelBall2x + getBola2().getVx());
+            getBola2().setVy(powerVelBall2y + getBola2().getVy());
+        }
     }
 } 
